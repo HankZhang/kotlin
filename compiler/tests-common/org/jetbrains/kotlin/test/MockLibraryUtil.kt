96c2937f95f99435cd0a947b8e84ecb9b83d03fa
Denis Zharkov <denis.zharkov@jetbrains.com>
2017-07-02 15:17:46 +0300

    Support package level annotations in Java
     #KT-10942 In Progress

--- compiler/tests-common/org/jetbrains/kotlin/test/MockLibraryUtil.kt
+++ compiler/tests-common/org/jetbrains/kotlin/test/MockLibraryUtil.kt
@@ -26,6 +26,7 @@ import org.jetbrains.kotlin.codegen.forTestCompile.ForTestCompileRuntime
 import org.jetbrains.kotlin.preloading.ClassPreloadingUtils
 import org.jetbrains.kotlin.preloading.Preloader
 import org.jetbrains.kotlin.utils.PathUtil
+import org.junit.Assert
 import org.junit.Assert.assertEquals
 import java.io.ByteArrayOutputStream
 import java.io.File
@@ -45,11 +46,29 @@ object MockLibraryUtil {
             sourcesPath: String,
             jarName: String,
             addSources: Boolean = false,
+            allowKotlinSources: Boolean = true,
             extraOptions: List<String> = emptyList(),
             extraClasspath: List<String> = emptyList()
     ): File {
         return compileLibraryToJar(
-                sourcesPath, KotlinTestUtils.tmpDir("testLibrary-" + jarName), jarName, addSources, extraOptions, extraClasspath
+                sourcesPath, KotlinTestUtils.tmpDir("testLibrary-" + jarName), jarName,
+                addSources, allowKotlinSources, extraOptions, extraClasspath
+        )
+    }
+
+    @JvmStatic
+    @JvmOverloads
+    fun compileJavaFilesLibraryToJar(
+            sourcesPath: String,
+            jarName: String,
+            addSources: Boolean = false,
+            extraOptions: List<String> = emptyList(),
+            extraClasspath: List<String> = emptyList()
+    ): File {
+        return compileJvmLibraryToJar(
+                sourcesPath, jarName, addSources,
+                allowKotlinSources = false,
+                extraClasspath = extraClasspath, extraOptions = extraOptions
         )
     }
 
@@ -60,6 +79,7 @@ object MockLibraryUtil {
             contentDir: File,
             jarName: String,
             addSources: Boolean = false,
+            allowKotlinSources: Boolean = true,
             extraOptions: List<String> = emptyList(),
             extraClasspath: List<String> = emptyList()
     ): File {
@@ -68,6 +88,7 @@ object MockLibraryUtil {
         val srcFile = File(sourcesPath)
         val kotlinFiles = FileUtil.findFilesByMask(Pattern.compile(".*\\.kt"), srcFile)
         if (srcFile.isFile || kotlinFiles.isNotEmpty()) {
+            Assert.assertTrue("Only java files are expected", allowKotlinSources)
             compileKotlin(sourcesPath, classesDir, extraOptions, *extraClasspath.toTypedArray())
         }
 
